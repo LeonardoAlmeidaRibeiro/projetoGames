6d4acfbb3b602162a3469ef6a4eb70a0236ac7e1
Leonardo <leonardo.a.ribeiro.21@gmail.com>
2025-05-25 13:43:21 -0300

    Primeiro commit

---
+++ src/main/java/com/projetogame/ribeiroleonardo/RibeiroleonardoApplication.java
@@ -0,0 +1,13 @@
+package com.projetogame.ribeiroleonardo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RibeiroleonardoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RibeiroleonardoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projetogame/ribeiroleonardo/RibeiroleonardoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetogame.ribeiroleonardo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RibeiroleonardoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
